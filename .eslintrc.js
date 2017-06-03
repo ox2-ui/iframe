b058eef3bc0c9d5c79444ab105cc811e59bac299
Lauricio Su <me@lauricio.com>
2017-06-03 19:10:20 +0700

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  extends: ['eslint:recommended','plugin:react/all', 'prettier', 'prettier/react'],
+  extends: ['eslint:recommended','plugin:react/all', 'plugin:import/errors', 'prettier', 'prettier/react'],
   parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 7,
@@ -18,48 +18,29 @@ module.exports = {
     }
   },
   plugins: [
-    'babel',
     'react',
     'jest',
     'import',
   ],
   rules: {
-    'arrow-parens': 'off',
+    'arrow-parens': ["error", "as-needed"],
+    'consistent-return': 'error',
     'consistent-this': ['error', 'self'],
-    'consistent-return': 'off', // Wishlist, one day
     'dot-notation': 'error',
-    'eqeqeq': ['error', 'smart'],
-    'indent': ['off', 2, {SwitchCase: 1}],
-    'id-blacklist': ['error', 'e'],
-    'max-len': 'off',
-    'new-cap': ['off', {capIsNew: true, newIsCap: true}], // Wishlist, one day
-    'no-unused-expressions': 'off', // do expressions in es7 https://github.com/babel/eslint-plugin-babel/issues/13
-    'no-unused-vars': 'error',
-    'no-shadow': 'off', // Wishlist, one day
-    'no-undef': 'error',
-    'no-empty-pattern': 'error',
-    'no-dupe-keys': 'error',
-    'no-dupe-args': 'error',
-    'no-duplicate-case': 'error',
-    'no-cond-assign': 'error',
-    'no-extra-boolean-cast': 'error',
-    'no-underscore-dangle': 'off',
+    'eqeqeq': ['error', 'always'],
+    'id-blacklist': ['error', 'e'], // B
+    'new-cap': ['error', {capIsNew: true, newIsCap: true}], // Wishlist, one day
+    'no-await-in-loop': 'error',
+    'no-shadow': 'error',
     'no-unneeded-ternary': 'error',
-    'no-unreachable': 'error',
+    'no-unused-expressions': 'off', // do expressions in es7 https://github.com/babel/eslint-plugin-babel/issues/13
     'no-var': 'error',
     'one-var': ['error', 'never'],
-    'operator-linebreak': ['off', 'after'],
-    'prefer-arrow-callback': 'off', // Wishlist, one day
+    'prefer-arrow-callback': 'error',
     'prefer-const': 'error',
     'prefer-template': 'error',
     'spaced-comment': 'error',
     'yoda': 'error',
-    'babel/object-curly-spacing': 'off',
-    'babel/arrow-parens': 'off',
-    'no-await-in-loop': 'error',
-    'strict': 'off',
-    'no-case-declarations': 'off',
-    'import/extensions': 'error',
     'react/forbid-component-props': 'off',
     'react/forbid-prop-types': 'off',
     'react/jsx-boolean-value': ['error', 'always'],
